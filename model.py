fff56621cc2dd9caf04dd22427f223d383e1181f
Ricardo Furbino <ricardofurbino@gmail.com>
2024-02-25 16:58:03 +0100

    refactor: makes change to not consider alphabet

--- model.py
+++ model.py
@@ -7,20 +7,20 @@ import pandas as pd
 from data_treatment import get_new_dataset_from_file
 
 
-def compute_scores(alphabet_file:str, train_file: str, test_file: str, r=4, n=10):
+def compute_scores(train_file: str, test_file: str, r=4, n=10, alphabet_file_path: str=''):
     return re.sub(
         "[b']",
         "",
         str(
             subprocess.check_output(
-                f"java -jar negsel2.jar -alphabet {alphabet_file} -self {train_file} -n {n} -r {r} -c -l < {test_file}",
+                f"java -jar negsel2.jar {f'-alphabet {alphabet_file_path}' if alphabet_file_path != '' else ''} -self {train_file} -n {n} -r {r} -c -l < {test_file}",
                 shell=True
             )
         )
     ).split('\\n')[:-1]
 
 
-def apply_model(alphaber_file_path, train_word_dataset, test_word_dataset, n, r):
+def apply_model(train_word_dataset, test_word_dataset, n, r, alphabet_file_path=""):
     """
     Apply the model.
     """
@@ -36,7 +36,7 @@ def apply_model(alphaber_file_path, train_word_dataset, test_word_dataset, n, r)
 
 
     # compute scores
-    result = compute_scores(alphaber_file_path, "train.train", "test.test", r, n)
+    result = compute_scores("train.train", "test.test", r, n, alphabet_file_path)
 
     return result
 
@@ -86,13 +86,13 @@ def save_anomally_score(label, output_file):
 
 
 def main(
-    alphaber_file,
     train_data_file,
     test_dataset_file,
     n,
     r,
     label_file,
     output_file,
+    alphabet_file = "",
     complete_character="_"
 ):
     """
@@ -105,7 +105,7 @@ def main(
     test_word_dataset, association_vetor = get_new_dataset_from_file(test_dataset_file, n, complete_character)
     
     print("Applying model...")
-    result = apply_model(alphaber_file, train_word_dataset, test_word_dataset, n, r)
+    result = apply_model(train_word_dataset, test_word_dataset, n, r, alphabet_file)
     
     print("Getting anomally score for each word...")
     label = get_anomally_score(result, association_vetor, label_file)
